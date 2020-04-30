aeb2b75a2062dd600bc6554160046646d44e5eec
vlad.serdyuk <vladserduk@live.com>
2020-04-30 21:43:38 +0300

    Add revirse string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+function reverse(str) {
+  if(str.length === 1) {
+    return str;
+  }
+
+  return str.slice(-1) + reverse(str.slice(0, -1));
+}
+
+function reverse(str) {
+  let result = '';
+  
+  for(let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+
+  return result;
+}
+
+function reverse(str) {
+  let result = '';
+  
+  for(let i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+
+  return result;
+}
 
 module.exports = reverse;
