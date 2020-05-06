f8e6e9c87c24c18ce13616729b5b13d199bc5e8c
vlad.serdyuk <vladserduk@live.com>
2020-05-06 22:58:02 +0300

    Add fibnacci solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,22 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
 
 module.exports = fib;
+
+/* function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+
+  return result[n];
+} */
