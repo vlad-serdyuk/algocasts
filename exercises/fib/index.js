fdecf3000db000bea63113e22b1042998b2e6978
vlad.serdyuk <vladserduk@live.com>
2020-05-07 22:46:39 +0300

    Add memoization to fib function

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,21 @@
 // Example:
 //   fib(4) === 3
 
+function memoization(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    
+    return result;
+  }
+}
+
 function fib(n) {
   if (n < 2) {
     return n;
@@ -15,6 +30,8 @@ function fib(n) {
 
   return fib(n - 1) + fib(n - 2);
 }
+
+fib = memoization(fib);
 
 module.exports = fib;
 
