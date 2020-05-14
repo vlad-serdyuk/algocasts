fdc4704f277cd23c52ec966fa2ee5eaf137ce1e7
vlad.serdyuk <vladserduk@live.com>
2020-05-14 12:04:43 +0300

    Add fromlast solution

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -12,7 +12,20 @@
 //    fromLast(list, 2).data // 'b'
 
 function fromLast(list, n) {
-  
+  let slow = list.head;
+  let fast = list.head;
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
 }
 
 module.exports = fromLast;
