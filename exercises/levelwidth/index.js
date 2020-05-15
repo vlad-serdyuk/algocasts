829ee5a2533c6bced4f93bdffafea5a2dbd30bd7
vlad.serdyuk <vladserduk@live.com>
2020-05-15 19:14:56 +0300

    Add levelWidth solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const levels = [0];
+  const delimiter = '&';
+  const nodes = [root, delimiter];
+
+  while(nodes.length > 1) {
+    const node = nodes.shift();
+
+    if (node === delimiter) {
+      levels.push(0);
+      nodes.push(delimiter);
+    } else {
+      if (node.children) {
+        nodes.push(...node.children);
+      }
+
+      levels[levels.length - 1]++;
+    }
+
+  }
+
+  return levels;
+}
 
 module.exports = levelWidth;
