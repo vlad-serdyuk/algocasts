54b6e08111f99dbf9a4f4b2c82aa79ee4e5c07d9
vlad.serdyuk <vladserduk@live.com>
2020-05-19 20:15:37 +0300

    Add bubbleSort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,17 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j+1]) {
+        let val = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = val;
+      }
+    }   
+  }
 
+  return arr;
 }
 
 function selectionSort(arr) {
