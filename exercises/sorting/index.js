87d48ab7626f38c7a9ee4b6d4510d131eafe580b
vlad.serdyuk <vladserduk@live.com>
2020-05-21 11:55:18 +0300

    Add mergeSort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -36,7 +36,15 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
